eaee1112b20981e28466e27198fb81437150dc3f
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-06-24 16:13:00 +0530

    ext

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const inputBtn=document.getElementById("input-btn")
-let myLeads=["AGA","BGA","CGA"]
+let myLeads=[]
 const inputEl=document.getElementById("input-el")
 const ulEl=document.getElementById("ul-el")
 const leads=document.getElementById("leads")
 
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value)
-
+    inputEl.value="" 
+    renderLeads()
 })
 
 function renderLeads(){  
@@ -17,7 +18,13 @@ for(let i=0;i<myLeads.length;i++)
         // const li=document.createElement("li")
         // li.textContent=myLeads[i]
         // ulEl.append(li)//or
-        listItems+= "<li>"+myLeads[i]+"</li> "
+        listItems+= `
+        <li>
+        <a target='_blank' href='${myLeads[i]}'>
+        ${myLeads[i]}
+        </a>
+        </li> 
+        `
        }
 
 ulEl.innerHTML=listItems
