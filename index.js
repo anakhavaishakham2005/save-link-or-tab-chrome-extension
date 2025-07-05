b13339441efd3dfc69c54d371de43f8913cf361d
anakhavaishakham2005 <anakhavaishakham2005@gmail.com>
2025-07-05 15:47:58 +0530

    new

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ let myLeads=[]
 const inputEl=document.getElementById("input-el")
 const ulEl=document.getElementById("ul-el")
 const leads=document.getElementById("leads")
-
+// localStorage.setItem("myLeads")
+// myLeads=JSON.parse(myLeads)//turn myLeads string to array
+// myLeads.push("www.instagram.com")//push new to array
+// myLeads=JSON.stringify(myLeads)//turn myLeads array to string
+leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value)
     inputEl.value="" 
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderLeads()
 })
 
